8609d8e4e2ff3cfc6dde345f490dd86e050eefab
xGamal <xgamaleldin@gmail.com>
2023-09-07 05:58:48 -0700

    update fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,13 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle, This is followed by a newline.
- * The return is  Always 0.
+ * puzzle, followed by a newline.
+ * Return: Always 0.
  */
 
+
 int main(void)
+
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
