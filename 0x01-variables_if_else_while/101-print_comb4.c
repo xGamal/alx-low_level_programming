30fc864bc41d664f53666c26bd413757654971b4
xGamal <xgamaleldin@gmail.com>
2023-09-09 05:42:06 -0700

    fix again 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,28 +7,28 @@
  */
 int main(void)
 {
-    int num1, num2, num3;
+	int num1, num2, num3;
 
-    for (num1 = 48; num1 < 58; num1++)
-    {
-        for (num2 = 49; num2 < 58; num2++)
-        {
-            for (num3 = 50; num3 < 58; num3++)
-            {
-                if (num3 > num2 && num2 > num1)
-                {
-                    putchar(num1);
-                    putchar(num2);
-                    putchar(num3);
-                    if (num1 != 55 || num2 != 56)
-                    {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                }
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (num1 = 48; num1 < 58; num1++)
+	{
+		for (num2 = 49; num2 < 58; num2++)
+		{
+			for (num3 = 50; num3 < 58; num3++)
+			{
+				if (num3 > num2 && num2 > num1)
+				{
+					putchar(num1);
+					putchar(num2);
+					putchar(num3);
+					if (num1 != 55 || num2 != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
