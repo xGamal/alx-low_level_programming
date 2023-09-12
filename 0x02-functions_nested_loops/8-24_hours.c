6886e27885655f35d88efbc45c2b1367f23ea70f
xGamal <xgamaleldin@gmail.com>
2023-09-12 15:46:59 -0700

    fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * print_last_digit - Prints the last digit of a number.
- * @n: The number whose last digit is to be printed.
- *
- * Return: The value of the last digit.
+ * jack_bauer - Prints every minute of the day of Jack Bauer,
+ *              from 00:00 to 23:59.
  */
-int print_last_digit(int n)
+void jack_bauer(void)
 {
-	int last_digit = n % 10;
+    int hour, minute;
 
-	if (last_digit < 0)
-	{
-		last_digit = -last_digit;
-	}
-
-	_putchar('0' + last_digit);
-	return (last_digit);
+    for (hour = 0; hour < 24; hour++)
+    {
+        for (minute = 0; minute < 60; minute++)
+        {
+            _putchar((hour / 10) + '0');
+            _putchar((hour % 10) + '0');
+            _putchar(':');
+            _putchar((minute / 10) + '0');
+            _putchar((minute % 10) + '0');
+            _putchar('\n');
+        }
+    }
 }
