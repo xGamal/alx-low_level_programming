2541695ebec5411f689d51c2800230954f6fa935
xGamal <xgamaleldin@gmail.com>
2023-09-12 16:14:13 -0700

    fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,33 @@
 #include "main.h"
-#include <stdio.h>
+
 
 /**
- * print_sign - Checks if n is positive, zero, or negative.
- * @n: The value to be checked.
- *
- * Return: 1 if n is positive, 0 if n is zero, -1 if n is negative.
- */
-int print_sign(int n)
+  * main - check the code.
+  *
+  * Return: Always 0.
+  */
+int main(void)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
+	int r;
+	r = print_sign(98);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0xff);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(-1);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
 }
